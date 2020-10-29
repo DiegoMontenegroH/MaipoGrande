663b0180359ea7ce4c4753ca8167fdb22273270e
diegopizzaa <dmontenegro.top@gmail.com>
2020-10-29 20:46:00 -0300

    Fusion Rama Manuel

---
+++ FrutOnlineAdmin/Presentancion/Formularios/Transportes.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Presentancion.Formularios
+{
+    partial class Transportes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRut_Transportista = new System.Windows.Forms.TextBox();
+            this.txtPatente = new System.Windows.Forms.TextBox();
+            this.txtTamanio = new System.Windows.Forms.TextBox();
+            this.txtRefigeracion = new System.Windows.Forms.TextBox();
+            this.txtCapacidad = new System.Windows.Forms.TextBox();
+            this.btnAgregarTransporte = new System.Windows.Forms.Button();
+            this.dgvTransportes = new System.Windows.Forms.DataGridView();
+            this.btnCargarTransportes = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransportes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(212, 383);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(85, 37);
+            this.btnModificar.TabIndex = 28;
+            this.btnModificar.Text = "Modificar Transporte";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 332);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 13);
+            this.label5.TabIndex = 27;
+            this.label5.Text = "Rut Transportista";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(45, 332);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Tamaño";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(380, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 13);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Refrigeracion Si/No";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(209, 291);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Capacidad";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 293);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 23;
+            this.label1.Text = "Patente";
+            // 
+            // txtRut_Transportista
+            // 
+            this.txtRut_Transportista.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtRut_Transportista.Location = new System.Drawing.Point(212, 348);
+            this.txtRut_Transportista.Name = "txtRut_Transportista";
+            this.txtRut_Transportista.Size = new System.Drawing.Size(100, 20);
+            this.txtRut_Transportista.TabIndex = 22;
+            // 
+            // txtPatente
+            // 
+            this.txtPatente.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtPatente.Location = new System.Drawing.Point(45, 309);
+            this.txtPatente.Name = "txtPatente";
+            this.txtPatente.Size = new System.Drawing.Size(100, 20);
+            this.txtPatente.TabIndex = 21;
+            // 
+            // txtTamanio
+            // 
+            this.txtTamanio.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtTamanio.Location = new System.Drawing.Point(45, 348);
+            this.txtTamanio.Name = "txtTamanio";
+            this.txtTamanio.Size = new System.Drawing.Size(100, 20);
+            this.txtTamanio.TabIndex = 20;
+            // 
+            // txtRefigeracion
+            // 
+            this.txtRefigeracion.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtRefigeracion.Location = new System.Drawing.Point(383, 309);
+            this.txtRefigeracion.Name = "txtRefigeracion";
+            this.txtRefigeracion.Size = new System.Drawing.Size(100, 20);
+            this.txtRefigeracion.TabIndex = 19;
+            // 
+            // txtCapacidad
+            // 
+            this.txtCapacidad.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtCapacidad.Location = new System.Drawing.Point(212, 309);
+            this.txtCapacidad.Name = "txtCapacidad";
+            this.txtCapacidad.Size = new System.Drawing.Size(100, 20);
+            this.txtCapacidad.TabIndex = 18;
+            // 
+            // btnAgregarTransporte
+            // 
+            this.btnAgregarTransporte.Location = new System.Drawing.Point(45, 383);
+            this.btnAgregarTransporte.Name = "btnAgregarTransporte";
+            this.btnAgregarTransporte.Size = new System.Drawing.Size(75, 37);
+            this.btnAgregarTransporte.TabIndex = 17;
+            this.btnAgregarTransporte.Text = "Agregar Transporte";
+            this.btnAgregarTransporte.UseVisualStyleBackColor = true;
+            // 
+            // dgvTransportes
+            // 
+            this.dgvTransportes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTransportes.Location = new System.Drawing.Point(45, 30);
+            this.dgvTransportes.Name = "dgvTransportes";
+            this.dgvTransportes.Size = new System.Drawing.Size(545, 258);
+            this.dgvTransportes.TabIndex = 16;
+            // 
+            // btnCargarTransportes
+            // 
+            this.btnCargarTransportes.Location = new System.Drawing.Point(636, 30);
+            this.btnCargarTransportes.Name = "btnCargarTransportes";
+            this.btnCargarTransportes.Size = new System.Drawing.Size(120, 66);
+            this.btnCargarTransportes.TabIndex = 15;
+            this.btnCargarTransportes.Text = "Cargar Transportes";
+            this.btnCargarTransportes.UseVisualStyleBackColor = true;
+            this.btnCargarTransportes.Click += new System.EventHandler(this.btnCargarTransportes_Click);
+            // 
+            // Transportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRut_Transportista);
+            this.Controls.Add(this.txtPatente);
+            this.Controls.Add(this.txtTamanio);
+            this.Controls.Add(this.txtRefigeracion);
+            this.Controls.Add(this.txtCapacidad);
+            this.Controls.Add(this.btnAgregarTransporte);
+            this.Controls.Add(this.dgvTransportes);
+            this.Controls.Add(this.btnCargarTransportes);
+            this.Name = "Transportes";
+            this.Text = "Transportes";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTransportes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRut_Transportista;
+        private System.Windows.Forms.TextBox txtPatente;
+        private System.Windows.Forms.TextBox txtTamanio;
+        private System.Windows.Forms.TextBox txtRefigeracion;
+        private System.Windows.Forms.TextBox txtCapacidad;
+        private System.Windows.Forms.Button btnAgregarTransporte;
+        private System.Windows.Forms.DataGridView dgvTransportes;
+        private System.Windows.Forms.Button btnCargarTransportes;
+    }
+}
